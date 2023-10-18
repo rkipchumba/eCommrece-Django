7a7d410c807821d19e7a98968146d281bb254dbe
Rodgers Kipchumba <kipchumbarodgers@gmail.com>
2023-10-18 11:03:27 +0300

    Checkout View

--- carts/urls.py
+++ carts/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import (
         cart_home, 
         cart_update,
+        checkout_home
 )
 
 urlpatterns = [
     path("", cart_home,name='home'),
+    path('checkout/', checkout_home, name="checkout"),
     path('update/', cart_update, name="update"),
 ] 
--- carts/views.py
+++ carts/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
-
+from django.shortcuts import render, redirect
+from orders.models import Order
 from products.models import Product
 from .models import Cart
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-   
     return render(request, "carts/home.html", {"cart": cart_obj})
 
 def cart_update(request):
@@ -15,15 +13,22 @@ def cart_update(request):
         try:
             product_obj = Product.objects.get(id=product_id)
         except Product.DoesNotExist:
-            print("Show message to user, product is gone?")
+            print("Show message to the user, product is gone?")
             return redirect("cart:home")
         cart_obj, new_obj = Cart.objects.new_or_get(request)
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
         else:
-            cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
+            cart_obj.products.add(product_obj)
         request.session['cart_items'] = cart_obj.products.count()
         # return redirect(product_obj.get_absolute_url())
     return redirect("cart:home")
 
-    
+def checkout_home(request):
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
+    order_obj = None
+    if cart_created or cart_obj.products.count() == 0:
+        return redirect("cart:home")
+    else:
+        order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+    return render(request, "carts/checkout.html", {"object": order_obj})
