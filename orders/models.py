00c8b5198efb85346ad3ed6faca40cbaa9c34941
Rodgers Kipchumba <kipchumbarodgers@gmail.com>
2023-10-18 12:03:27 +0300

    Decimals and float

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,11 @@
+import math
+
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from carts.models import Cart
 from ecommerce.utils import unique_order_id_generator
+
+
 
 ORDER_STATUS_CHOICES = (
     ('created', 'Created'),
@@ -26,8 +30,10 @@ class Order(models.Model):
     def update_total(self):
         cart_total = self.cart.total
         shipping_total = self.shipping_total
-        new_total = cart_total + shipping_total
-        self.total = new_total
+        new_total = math.fsum([cart_total, shipping_total])
+        formated_total = format(new_total, '.2f')
+        # new_total = cart_total + shipping_total
+        self.total = formated_total
         self.save()
         return new_total
         
