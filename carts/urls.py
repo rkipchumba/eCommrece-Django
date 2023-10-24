f0472389a5bbd605a7a7a1ab37f5a2d21a3adb22
Rodgers Kipchumba <kipchumbarodgers@gmail.com>
2023-10-24 14:53:18 +0300

    Checkout Success

--- carts/urls.py
+++ carts/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from .views import (
         cart_home, 
         cart_update,
-        checkout_home
+        checkout_home,
+        checkout_done_view
 )
 
 urlpatterns = [
     path("", cart_home,name='home'),
+    path('checkout/success/', checkout_done_view, name="success"),
     path('checkout/', checkout_home, name="checkout"),
     path('update/', cart_update, name="update"),
 ] 
--- carts/views.py
+++ carts/views.py
@@ -68,7 +68,7 @@ def checkout_home(request):
             order_obj.mark_paid()
             request.session['cart_items'] = 0
             del request.session['cart_id']
-            return redirect("/cart/success")
+            return redirect("cart:success")
 
     context = {
         "object": order_obj,
@@ -80,3 +80,6 @@ def checkout_home(request):
     }
 
     return render(request, "carts/checkout.html", context)
+
+def checkout_done_view(request):
+    return render(request, "carts/checkout-done.html", {})
