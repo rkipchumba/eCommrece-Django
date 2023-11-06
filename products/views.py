e2fd8d5e8f62233a89775b536225365d0cf82345
Rodgers Kipchumba <kipchumbarodgers@gmail.com>
2023-11-06 15:21:17 +0300

    Object Viewed Mixin

--- products/views.py
+++ products/views.py
@@ -3,6 +3,8 @@ from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
+from analytics.mixins import ObjectViewedMixin
+
 from carts.models import Cart
 
 from .models import Product
@@ -16,7 +18,7 @@ class ProductFeaturedListView(ListView):
         request = self.request
         return Product.objects.all().featured()
 
-class ProductFeaturedDetailView(DetailView):
+class ProductFeaturedDetailView(ObjectViewedMixin, DetailView):
     queryset = Product.objects.all().featured()
     template_name = "products/featured-detail.html"
 
@@ -52,7 +54,7 @@ def product_list_view(request):
     }
     return render(request, 'products/list.html', context)
 
-class ProductDetailSlugView(DetailView):
+class ProductDetailSlugView(ObjectViewedMixin, DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail.html"
     
@@ -78,7 +80,7 @@ class ProductDetailSlugView(DetailView):
         return instance
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(ObjectViewedMixin, DetailView):
     # queryset = Product.objects.all() 
     template_name = "products/detail.html"
 
