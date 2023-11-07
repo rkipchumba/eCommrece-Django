70883717131786c3f122d84dd0c1f6762953b2dd
Rodgers Kipchumba <kipchumbarodgers@gmail.com>
2023-11-07 14:18:19 +0300

    Payment Method Improvement

--- billing/views.py
+++ billing/views.py
@@ -1,13 +1,38 @@
+# from django.shortcuts import render
+# from django.http import JsonResponse, HttpResponse
+
+# import stripe
+# stripe.api_key = "sk_test_51N7Ev4GCBBy71y1GvMTAGNwEC9c689VlZdTCVkWm4jTliTx89h5qzJtDdUC1gEZ3jPUhL7eLWZ7H3H1GJshuYhPL00QWQzC99y"
+
+
+# STRIPE_PUB_KEY = 'pk_test_51N7Ev4GCBBy71y1Gz612DmzcqFfy0lsrzhhFQCwjsMmWJ9rJ5l4UQVHWM5sUtuszwQJdG9dlmuKABULDxzXvgHWf00W14nXBjf'
+
+
+# def payment_method_view(request):
+#     # if request.method == "POST":
+#     #     print(request.POST)
+#     return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY})
+
+# def payment_method_createview(request):
+#     if request.method == "POST" and request.is_ajax():
+#         print(request.POST)
+#         return JsonResponse({"message": "Done"})
+#     return HttpResponse("error", status=401)  
+
+
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 
 import stripe
 stripe.api_key = "sk_test_51N7Ev4GCBBy71y1GvMTAGNwEC9c689VlZdTCVkWm4jTliTx89h5qzJtDdUC1gEZ3jPUhL7eLWZ7H3H1GJshuYhPL00QWQzC99y"
 
-
 STRIPE_PUB_KEY = 'pk_test_51N7Ev4GCBBy71y1Gz612DmzcqFfy0lsrzhhFQCwjsMmWJ9rJ5l4UQVHWM5sUtuszwQJdG9dlmuKABULDxzXvgHWf00W14nXBjf'
 
-
 def payment_method_view(request):
-    if request.method == "POST":
-        print(request.POST)
     return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY})
+
+def payment_method_createview(request):
+    if request.method == "POST" and request.is_ajax():
+        print(request.POST)
+        return JsonResponse({"message": "Done"})
+    return HttpResponse("error", status=401)  # Use 'status' instead of 'status_code'
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -24,7 +24,7 @@ from django.views.generic import TemplateView
 
 from accounts.views import LoginView, RegisterView, guest_register_view
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view 
-from billing.views import payment_method_view
+from billing.views import payment_method_view, payment_method_createview
 from carts.views import cart_detail_api_view
 
 from .views import home_page, about_page, contact_page
@@ -41,6 +41,7 @@ urlpatterns = [
     path('api/cart/', cart_detail_api_view, name='api-cart'),
     path('cart/', include(('carts.urls', 'carts'), namespace='cart')),
     path("billing/payment-method/", payment_method_view, name='billing-payment-method'),
+    path("billing/payment-method/create", payment_method_createview, name='billing-payment-method-endpoint'),
 
     path("register/", RegisterView.as_view(), name='register'),
     path("bootstrap/", TemplateView.as_view(template_name='bootstrap/example.html')), 
